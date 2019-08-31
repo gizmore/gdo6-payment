760430d94d1a764380fdcf8a9aa7e42bf072faec
gizmore <gizmore@wechall.net>
2019-08-31 16:28:39 +0200

    6.10-r42_german_lang

---
+++ lang/payment_de.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+#############################################################
+'price' => 'Preis',
+#############################################################
+'err_order' => 'Deine Bestellung konnte nicht gefunden werden.',
+'err_orderable' => 'Bitte wählen Sie zuerst einen Artikel aus.',
+#############################################################
+'card_title_order' => 'Ihre Bestellung',
+'payment' => 'Zahlungsweise',
+'payment_fee' => 'Gebühr(%)',
+'total' => 'Gesamtsumme',
+#############################################################
+'msg_order_execute' => 'Ihre Bestellung wurde erfolgreich ausgeführt.',
+);
