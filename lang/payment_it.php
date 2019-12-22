8e1c3de12294470ce53ff9b5639ccbe21515744e
gizmore <gizmore@wechall.net>
2019-12-22 18:58:48 +0100

    6.10-r1042_it

---
+++ lang/payment_it.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+'order_title' => 'Titolo ordianto',
+'paid_at' => 'Pagato il',
+'executed_at' => 'effetuato il',
+#############################################################
+'price' => 'Prezzo',
+#############################################################
+'err_order' => 'Il vostro ordine non é stato trovato.',
+'err_orderable' => 'La preghiamo di selezionare prima un articolo.',
+#############################################################
+'card_title_order' => 'Il vostro ordine',
+'payment' => 'Metodo di pagamento',
+'payment_fee' => 'Cannone (%)',
+'total' => 'Somma totale',
+#############################################################
+'msg_order_execute' => 'Il vostro ordine é stato effettuato con successo.',
+#############################################################
+'ft_payment_order' => 'Ordine',
+'btn_execute' => 'Effettuare',
+#############################################################
+'link_your_orders' => 'Vostri ordini',
+);
